c6c4503231caa3e5c67a9e2c87ebd4e92be0db63
Domiziano De Santis <119798750+DeSantisDomiziano@users.noreply.github.com>
2023-09-18 15:39:38 +0200

    add getter, setter & toString

--- src/org/shop/java/Main.java
+++ src/org/shop/java/Main.java
@@ -1,5 +1,12 @@
 package org.shop.java;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+		Prodotto p1 = new Prodotto("ciao", "bella", 22.10F, 5);
+		Prodotto p2 = new Prodotto("ciao", "bella", 22.10F, 5);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+	}
 }
--- src/org/shop/java/Prodotto.java
+++ src/org/shop/java/Prodotto.java
@@ -1,5 +1,71 @@
 package org.shop.java;
 
-public class Prodotto {
+import java.util.Random;
 
+public class Prodotto {
+	
+	private String code;
+	private String name;
+	private String brand;
+	private float price;
+	private int vat;
+	
+	public Prodotto(String name, String brand, float price, int vat) {
+		setCode();
+		setName(name);
+		setBrand(brand);
+		setPrice(price);
+		setVat(vat);
+	}
+	
+	//TUTTI I GETTER
+	public String getCode() {
+		return code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	
+	public int getVat() {
+		return vat;
+	}
+	
+	//TUTTI I SETTER
+	private void setCode() {
+		code = "productCode##";
+	}
+	
+	public void setName(String name) {
+		if(name.length() > 0)
+			this.name = name;
+	}
+	
+	public void setBrand(String brand) {
+		if(brand.length() > 0)
+			this.brand = brand;
+	}
+	
+	public void setPrice(float price) {
+		if(price > 0 && price < 10_000)
+			this.price = price;
+	}
+	
+	public void setVat(int vat) {
+		if(vat > 0 && vat < 128)
+			this.vat = vat;
+	}
+	
+	@Override
+	public String toString() {
+		return "code: " + code + " | name: " + name + " | brand: " + brand + " | price: " + price + " | vat: " + vat;
+	}
 }
